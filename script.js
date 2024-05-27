d487cccf8a6f7e8856423c8913ed13cb004eb475
JoaMcJoa <ajmcjoa@gmail.com>
2024-05-27 16:31:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,501 @@
+monthsDict = {
+  January: "Januarie",
+  February: "Februarie",
+  March: "Maart",
+  April: "April",
+  May: "Mei",
+  June: "Junie",
+  July: "Julie",
+  August: "Augustus",
+  September: "September",
+  October: "Oktober",
+  November: "November",
+  December: "Desember",
+};
+
+daysDict = {
+  Sunday: "Sondag",
+  Monday: "Maandag",
+  Tuesday: "Dinsdag",
+  Wednesday: "Woensdag",
+  Thursday: "Donderdag",
+  Friday: "Vrydag",
+  Saturday: "Saterdag",
+};
+
+seasonsDict = {
+  spring: "lente",
+  summer: "sommer",
+  autumn: "herfs",
+  winter: "winter",
+};
+
+mealsDict = {
+  breakfast: "ontbyt",
+  lunch: "middagete",
+  dinner: "aandete",
+  "tea-time": "teetyd",
+  snack: "peuselhappie",
+  cocktails: "skemerkelkie",
+};
+
+timesDict = {
+  afternoon: "middag",
+  day: "dag",
+  evening: "aand",
+  "last week": "verlede week",
+  midday: "middag",
+  midnight: "middernag",
+  morning: "oggend",
+  "next week": "aanstaande week",
+  night: "nag",
+  sunrise: "sonop",
+  sunset: "sonsondergang",
+  today: "vandag",
+  tomorrow: "more",
+  yesterday: "gister",
+  weekend: "naweek",
+};
+
+coloursDict = {
+  beige: "beige",
+  black: "swart",
+  blue: "blou",
+  brown: "bruin",
+  cream: "room",
+  "dark ...": "donker ...",
+  gold: "goud",
+  green: "groen",
+  grey: "grys",
+  "light ...": "lig ...",
+  maroon: "bruinrooi",
+  "navy blue": "donkerblou",
+  orange: "oranje",
+  pink: "pienk",
+  purple: "pers",
+  red: "rooi",
+  silver: "silwer",
+  white: "wit",
+  yellow: "geel",
+};
+
+familyDict = {
+  aunt: "tante",
+  brother: "broer",
+  "brother-in-law": "swaer",
+  "male cousin": "neef",
+  "female cousin": "niggie",
+  daughter: "dogter",
+  father: "pa",
+  "father-in-law": "skoonpa",
+  godfather: "peetvader",
+  godmother: "peetmoeder",
+  grandfather: "oupa",
+  grandmother: "ouma",
+  grandparents: "grootouers",
+  mother: "ma",
+  "mother-in-law": "skoonma",
+  nephew: "neef",
+  niece: "niggie",
+  parents: "ouers",
+  sister: "suster",
+  "sister-in-law": "skoonsuster",
+  son: "seun",
+  stepdaughter: "stiefdogter",
+  stepfather: "stiefvader",
+  stepmother: "stiefmoeder",
+  stepson: "stiefseun",
+  uncle: "oom",
+};
+
+roomsDict = {
+  attic: "solder",
+  balcony: "balkon",
+  basement: "kelder",
+  bathroom: "badkamer",
+  bedroom: "slaapkamer",
+  "dining room": "eetkamer",
+  "entrance hall": "portaal",
+  "family room": "gesinskamer",
+  garage: "motorhuis",
+  kitchen: "kombuis",
+  lounge: "sitkamer",
+  pantry: "spens",
+  passage: "gang",
+  "spare room": "gastekamer",
+  study: "studeerkamer",
+  toilet: "toilet",
+  verander: "stoep",
+};
+
+furnitureDict = {
+  "arm chair": "leunstoel",
+  bed: "bed",
+  "bedroom suite": "slaapkamer stel",
+  carpet: "tapyt",
+  chair: "stoel",
+  couch: "bank",
+  desk: "lessenaar",
+  "diningroom suite": "eetkamerstel",
+  "lounge suite": "sitkamerstel",
+  paintings: "skilderye",
+  pictures: "prente",
+  table: "tafel",
+};
+
+appliancesDict = {
+  "alarm clock": "wekker",
+  cellphone: "selfoon",
+  freezer: "vrieskas",
+  fridge: "yskas",
+  "hair dryer": "haardroer",
+  iron: "strykyster",
+  kettle: "ketel",
+  "microwave oven": "mikrogolfoond",
+  razor: "skeermes",
+  stove: "stoof",
+  television: "televisie",
+  toaster: "broodrooster",
+  "vacuum cleaner": "stofsuier",
+  "washing machine": "wasmasjien",
+};
+
+householdDict = {
+  blankets: "komberse",
+  crockery: "breekgoed",
+  cups: "koppies",
+  curtains: "gordyne",
+  cutlery: "eetgerei",
+  forks: "vurke",
+  glasses: "glase",
+  knives: "messe",
+  lamp: "lamp",
+  pillows: "kussings",
+  plates: "borde",
+  saucers: "pierings",
+  sheets: "lakens",
+  spoons: "lepels",
+  teaspoons: "teelepels",
+  towels: "handdoeke",
+};
+
+clothesDict = {
+  belt: "gordel",
+  blouse: "bloes",
+  boots: "stewels",
+  cap: "pet",
+  coat: "jas",
+  dress: "rok",
+  "dressing gown": "kamerjas",
+  "evening wear": "aanddrag",
+  gloves: "handskoene",
+  handbag: "handsak",
+  handkerchief: "sakdoek",
+  hat: "hoed",
+  jacket: "baadjie",
+  jersey: "trui",
+  nightdress: "nagrok",
+  pantiehose: "kousbroekie",
+  pyjamas: "nagklere",
+  raincoat: "reenjas",
+  sandals: "sandale",
+  shirt: "hemp",
+  shoes: "skoene",
+  shorts: "kortbroek",
+  skirt: "romp",
+  socks: "kouse",
+  suit: "n pak klere",
+  scarf: "serp",
+  sunglasses: "sonbril",
+  "swimming costume": "swembroek",
+  takkies: "tekkies",
+  tie: "das",
+  tracksuit: "sweetpak",
+  umbrella: "sambreel",
+  underpants: "onderbroek",
+};
+
+werkwoordeDict = {
+  accept: "aanvaar",
+  achieve: "bereik",
+  act: "gedra",
+  admit: "erken",
+  affect: "beinvloed",
+  afford: "bekostig",
+  agree: "stem saam",
+  answer: "antwoord",
+  argue: "stry",
+  attach: "haak aan",
+  allow: "laat toe",
+  arrive: "arriveer",
+  ask: "vra",
+  avoid: "vermy",
+  become: "word",
+  begin: "begin",
+  believe: "glo",
+  break: "breek",
+  call: "roep/bel/oproep",
+  can: "kan",
+  care: "gee om",
+  carry: "dra",
+  change: "verander",
+  compare: "vergelyk",
+  concern: "bekommer",
+  consider: "oorweeg",
+  contribute: "dra by",
+  cook: "kook",
+  correct: "korrigeer",
+  create: "skep/maak",
+  cut: "sny",
+  choose: "kies",
+  clean: "maak skoon",
+  climb: "klim",
+  close: "maak toe",
+  collect: "versamel",
+  complain: "kla",
+  confirm: "bevestig",
+  continue: "gaan aan",
+  control: "beheer",
+  could: "kon",
+  count: "tel",
+  cry: "huil",
+  damage: "beskadig",
+  dance: "dans",
+  decide: "besluit",
+  deliver: "lewer af",
+  deny: "ontken",
+  depend: "hang af",
+  describe: "beskryf",
+  design: "ontwerp",
+  destroy: "vernietig",
+  develop: "ontwerp",
+  die: "sterf",
+  disappear: "verdwyn",
+  discover: "ontdek",
+  discuss: "bespreek",
+  divide: "verdeel",
+  do: "doen",
+  draw: "teken",
+  dress: "trek aan",
+  drink: "drink",
+  drive: "bestuur",
+  drop: "laat val",
+  eat: "eet",
+  encourage: "moedig aan",
+  end: "einde",
+  enjoy: "geniet",
+  examine: "bestudeer",
+  expect: "verwag",
+  experience: "ervaar",
+  explain: "verduidelik",
+  express: "druk uit",
+};
+
+jewelleryDict = {
+  bracelet: "armband",
+  brooch: "borsspeld",
+  chain: "ketting",
+  "engagement ring": "verloofring",
+  locket: "hangertjie",
+  necklace: "halssnoer",
+  pearls: "perels",
+  rings: "ringe",
+  watch: "horlosie",
+};
+
+topics = [
+  "maande",
+  "dae",
+  "seisoenne",
+  "maaltye",
+  "tye",
+  "kleure",
+  "familielede",
+  "kamers",
+  "meubles",
+  "toestelle",
+  "huishoudelike items",
+  "kleure",
+  "werkwoorde",
+  "juweliersware"
+];
+topicsSet = new Set(topics);
+topicsStr = "'maande', 'dae', 'seisoenne', 'maaltye', 'tye', 'kleure', 'familielede', 'kamers', 'meubles', 'toestelle', 'huishoudelike items', 'kleure', 'werkwoorde' en 'juweliersware'";
+
+topicsDialog = "Die vakke wat ek ken is ... ".concat(topicsStr);
+
+function test() {
+  if (topicsSet.has("werkwoorde")) {
+    document.getElementById("teacherDialog").innerHTML = topicsDialog;
+  }
+}
+
+tgtDict = werkwoordeDict;
+
+let initialDialog = 0;
+let quNumber = 0;
+let answerGiven = 0;
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    // Generate random number
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+function next() {
+  studentAnswer = document.getElementById("studentAnswer").value;
+
+  if (initialDialog == 0) {
+    if (topicsSet.has(studentAnswer)) {
+      if (document.getElementById("studentAnswer").value == "maande") {
+        tgtDict = monthsDict;
+      }
+      if (document.getElementById("studentAnswer").value == "dae") {
+        tgtDict = daysDict;
+      }
+      if (document.getElementById("studentAnswer").value == "seisoenne") {
+        tgtDict = seasonsDict;
+      }
+      if (document.getElementById("studentAnswer").value == "maaltye") {
+        tgtDict = mealsDict;
+      }
+      if (document.getElementById("studentAnswer").value == "tye") {
+        tgtDict = timesDict;
+      }
+      if (document.getElementById("studentAnswer").value == "kleure") {
+        tgtDict = coloursDict;
+      }
+      if (document.getElementById("studentAnswer").value == "meubles") {
+        tgtDict = furnitureDict;
+      }
+      if (document.getElementById("studentAnswer").value == "toestelle") {
+        tgtDict = appliancesDict;
+      }
+      if (
+        document.getElementById("studentAnswer").value == "huishoudelike items"
+      ) {
+        tgtDict = householdDict;
+      }
+      if (document.getElementById("studentAnswer").value == "werkwoorde") {
+        tgtDict = clothesDict;
+      }
+      if (document.getElementById("studentAnswer").value == "werkwoorde") {
+        tgtDict = werkwoordeDict;
+      }
+      if (document.getElementById("studentAnswer").value == "werkwoorde") {
+        tgtDict = jewelleryDict;
+      }
+
+      initialDialog += 1;
+
+      tgtKeys = Object.keys(tgtDict);
+      shuffleTgtArray = shuffleArray(tgtKeys);
+
+      teacherQu = "Die Engelse word is '".concat(
+        shuffleTgtArray[quNumber],
+        "'. Wat beteken dit in Afrikaans?"
+      );
+      document.getElementById("teacherDialog").innerHTML = teacherQu;
+
+      quNumber += 1;
+      answerGiven = 0;
+      document.getElementById("studentAnswer").value = "";
+    } else {
+      document.getElementById("teacherDialog").innerHTML = topicsDialog;
+    }
+  } else {
+    if (answerGiven == 1) {
+      teacherQu = "Die Engelse word is ... '".concat(
+        shuffleTgtArray[quNumber],
+        "'"
+      );
+      document.getElementById("teacherDialog").innerHTML = teacherQu;
+
+      quNumber += 1;
+      document.getElementById("studentAnswer").value = "";
+
+      answerGiven = 0;
+    }
+  }
+}
+
+function check() {
+  studentAnswer = document.getElementById("studentAnswer").value;
+
+  if (studentAnswer != "") {
+    if (answerGiven == 0) {
+      correctAnswer = tgtDict[shuffleTgtArray[quNumber - 1]];
+      studentAnswer = document.getElementById("studentAnswer").value;
+
+      if (studentAnswer == correctAnswer) {
+        document.getElementById("teacher").src = "happy_teacher_once_off.gif";
+        happyDialog = "Baaie mooi! Dit is reg. '".concat(
+          correctAnswer,
+          "' beteken '",
+          shuffleTgtArray[quNumber - 1],
+          "' in Engels. Kom ons probeer 'n nogge vraag."
+        );
+        document.getElementById("teacherDialog").innerHTML = happyDialog;
+        document.getElementById("studentAnswer").value = "";
+      } else {
+        document.getElementById("teacher").src = "sad_teacher_once_off.gif";
+        sadDialog = "Jammer! Die antwoord was ... '".concat(
+          correctAnswer,
+          "'. Kom ons probeer 'n nogge vraag."
+        );
+        document.getElementById("teacherDialog").innerHTML = sadDialog;
+        document.getElementById("studentAnswer").value = "";
+      }
+    }
+
+    answerGiven = 1;
+  }
+}
+
+/* const tgtKeys = Object.keys(tgtDict);
+const randomKey = tgtKeys[Math.floor(Math.random() * tgtKeys.length)]; */
+
+/* function before() {
+  document.getElementById("teacher").src = "teacher.png";
+  let englishWord = shuffleTgtArray[questionsAsked];
+  let answerWord = shuffleTgtArray[questionsAsked - 1];
+  let correctAnswer = tgtDict[answerWord];
+  let teacherQu = "'".concat(englishWord, "'");
+  document.getElementById("teacherDialog").innerHTML = teacherQu;
+  questionsAsked += 1;
+}
+
+questionsAsked = 0;
+
+function after() {
+  let englishWord = shuffleTgtArray[questionsAsked];
+  let answerWord = shuffleTgtArray[questionsAsked - 1];
+  let correctAnswer = tgtDict[answerWord];
+  let teacherQu = "'".concat(englishWord, "'");
+  document.getElementById("teacherDialog").innerHTML = teacherQu;
+  let studentAnswer = document.getElementById("studentAnswer").value;
+  if (studentAnswer == correctAnswer) {
+    document.getElementById("teacher").src = "happy_teacher_once_off.gif";
+    let happyDialog = "Baaie mooi! Dit is reg. '".concat(
+      correctAnswer,
+      "' beteken '",
+      answerWord,
+      "'"
+    );
+    document.getElementById("teacherDialog").innerHTML = happyDialog;
+    document.getElementById("teacherStudentDialog").reset();
+  } else {
+    document.getElementById("teacher").src = "sad_teacher.png";
+    let sadDialog = "Jammer! Die antwoord was ..... '".concat(
+      correctAnswer,
+      "'"
+    );
+    document.getElementById("teacherDialog").innerHTML = sadDialog;
+    document.getElementById("teacherStudentDialog").reset();
+  }
+} */
